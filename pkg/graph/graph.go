a2e1dabc008495aacc76a737e6d25a097e5f253c
Steve Teuber <github@steveteuber.com>
2020-04-08 22:27:53 +0200

    Move currently existing core resources to seperate file

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -177,29 +177,10 @@ func (g *Graph) Unstructured(unstr *unstructured.Unstructured) (err error) {
 	g.Node(unstr.GroupVersionKind(), unstr)
 
 	switch unstr.GetAPIVersion() {
+	case "v1":
+		return g.CoreV1().Unstructured(unstr)
 	case "networking.k8s.io/v1":
 		return g.NetworkingV1().Unstructured(unstr)
-	}
-
-	switch unstr.GetKind() {
-	case "Pod":
-		obj := &v1.Pod{}
-		if err = FromUnstructured(unstr, obj); err != nil {
-			return err
-		}
-		_, err = g.Pod(obj)
-	case "Endpoints":
-		obj := &v1.Endpoints{}
-		if err = FromUnstructured(unstr, obj); err != nil {
-			return err
-		}
-		_, err = g.Endpoints(obj)
-	case "Service":
-		obj := &v1.Service{}
-		if err = FromUnstructured(unstr, obj); err != nil {
-			return err
-		}
-		_, err = g.Service(obj)
 	}
 
 	return err
@@ -272,121 +253,4 @@ func (g Graph) Write(w io.Writer, format string) error {
 	}
 
 	return nil
-}
-
-// Pod adds a v1.Pod resource to the Graph.
-func (g *Graph) Pod(pod *v1.Pod) (*Node, error) {
-	n := g.Node(schema.FromAPIVersionAndKind(v1.GroupName, "Pod"), pod)
-
-	for _, container := range pod.Spec.Containers {
-		c, err := g.Container(pod, container)
-		if err != nil {
-			return nil, err
-		}
-		g.Relationship(n, "Container", c)
-	}
-
-	return n, nil
-}
-
-// Container adds a v1.Container resource to the Graph.
-func (g *Graph) Container(pod *v1.Pod, container v1.Container) (*Node, error) {
-	n := g.Node(
-		schema.FromAPIVersionAndKind(v1.GroupName, "Container"),
-		&metav1.ObjectMeta{
-			UID:       ToUID(string(pod.GetUID()), container.Name),
-			Namespace: pod.GetNamespace(),
-			Name:      container.Name,
-		},
-	)
-	i, err := g.ContainerImage(v1.ContainerImage{Names: []string{container.Image}})
-	if err != nil {
-		return nil, err
-	}
-	g.Relationship(n, "ContainerImage", i)
-
-	return n, nil
-}
-
-// ContainerImage adds a v1.ContainerImage resource to the Graph.
-func (g *Graph) ContainerImage(image v1.ContainerImage) (*Node, error) {
-	n := g.Node(
-		schema.FromAPIVersionAndKind(v1.GroupName, "ContainerImage"),
-		&metav1.ObjectMeta{
-			UID:  ToUID(strings.Join(image.Names, "-")),
-			Name: strings.Join(image.Names, ","),
-		},
-	)
-
-	return n, nil
-}
-
-// Endpoints adds a v1.Endpoints resource to the Graph.
-func (g *Graph) Endpoints(obj *v1.Endpoints) (*Node, error) {
-	n := g.Node(schema.FromAPIVersionAndKind(v1.GroupName, "Endpoints"), obj)
-
-	for _, subset := range obj.Subsets {
-		for _, address := range subset.Addresses {
-			t := g.Node(
-				address.TargetRef.GroupVersionKind(),
-				&metav1.ObjectMeta{
-					UID:       address.TargetRef.UID,
-					Name:      address.TargetRef.Name,
-					Namespace: address.TargetRef.Namespace,
-				},
-			)
-			g.Relationship(n, address.TargetRef.Kind, t)
-		}
-	}
-
-	return n, nil
-}
-
-// Service adds a v1.Service resource to the Graph.
-func (g *Graph) Service(obj *v1.Service) (*Node, error) {
-	switch obj.Spec.Type {
-	case v1.ServiceTypeClusterIP:
-		return g.ServiceTypeClusterIP(obj)
-		// case v1.ServiceTypeNodePort:
-		// case v1.ServiceTypeLoadBalancer:
-	case v1.ServiceTypeExternalName:
-		return g.ServiceTypeExternalName(obj)
-	}
-
-	return nil, nil
-}
-
-// ServiceTypeClusterIP adds a v1.Service of type ClusterIP to the Graph.
-func (g *Graph) ServiceTypeClusterIP(obj *v1.Service) (*Node, error) {
-	n := g.Node(obj.GroupVersionKind(), obj)
-
-	options := metav1.GetOptions{}
-	endpoints, err := g.clientset.CoreV1().Endpoints(obj.GetNamespace()).Get(obj.GetName(), options)
-	if err != nil {
-		return nil, err
-	}
-
-	e, err := g.Endpoints(endpoints)
-	if err != nil {
-		return nil, err
-	}
-	g.Relationship(n, "Endpoints", e)
-
-	return n, nil
-}
-
-// ServiceTypeExternalName adds a v1.Service of type ExternalName to the Graph.
-func (g *Graph) ServiceTypeExternalName(obj *v1.Service) (*Node, error) {
-	n := g.Node(obj.GroupVersionKind(), obj)
-
-	e := g.Node(
-		schema.FromAPIVersionAndKind(v1.GroupName, "ExternalName"),
-		&metav1.ObjectMeta{
-			UID:  ToUID(obj.Spec.ExternalName),
-			Name: obj.Spec.ExternalName,
-		},
-	)
-	g.Relationship(n, "ExternalName", e)
-
-	return n, nil
 }
